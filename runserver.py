d6c56015356f7472e75e8f727e41e7266ee1f1a8
John Cleaver <cleaver.john.k@gmail.com>
2016-03-13 16:16:55 -0400

    Added a file to run development server.

---
+++ runserver.py
@@ -0,0 +1,10 @@
+"""
+
+Author:    John Cleaver <cleaver.john.k@gmail.com>
+Copyright: 2016 John Cleaver
+License:   See LICENSE file
+"""
+
+from stardew_community_checklist import app
+
+app.run(debug=True)
