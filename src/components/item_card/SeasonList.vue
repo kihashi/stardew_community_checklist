9710464090a984e321d55ac6eb025d44f2fd8910
John Cleaver <cleaver.john.k@gmail.com>
2018-02-27 21:56:07 -0500

    Add season list

--- src/components/item_card/SeasonList.vue
+++ src/components/item_card/SeasonList.vue
@@ -1,11 +1,28 @@
 <template>
-
+  <span class="tags">
+    <season-tag v-for="season in seasons" :key="SeasonOrder(season)" :season="season"/>
+  </span>
 </template>
 
 <script>
-    export default {
-        name: "season-list"
+import SeasonTag from './SeasonTag'
+import SeasonData from '@/assets/game_data/seasons.json'
+
+export default {
+  components: {SeasonTag},
+  name: 'season-list',
+  props: {
+    seasons: {
+      type: Array,
+      required: true
     }
+  },
+  methods: {
+    SeasonOrder: function (value) {
+      return SeasonData[value].id
+    }
+  }
+}
 </script>
 
 <style scoped>
