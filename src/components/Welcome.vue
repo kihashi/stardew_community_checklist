0c7248064c47a2727918df117b6329cd59da2b65
John Cleaver <cleaver.john.k@gmail.com>
2018-02-27 21:56:23 -0500

    Remove background from welcome for better look.

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="hero is-success is-medium is-bold">
+    <section class="hero is-medium is-bold">
       <div class="hero-body">
         <h1 class="title">
           Stardew Community Checklist
