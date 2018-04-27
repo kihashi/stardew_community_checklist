d912e9eb483cf51685da546168739b6687d356dd
John Cleaver <cleaver.john.k@gmail.com>
2018-04-26 22:53:41 -0400

    Add navigation and bundle display area.

--- src/components/Bundles.vue
+++ src/components/Bundles.vue
@@ -1,13 +1,16 @@
 <template>
-
+  <div>
+    <BundleNav />
+    <section class="section">
+      <router-view></router-view>
+    </section>
+  </div>
 </template>
 
 <script>
+import BundleNav from '@/components/BundleNav.vue'
 export default {
+  components: {BundleNav},
   name: 'bundles'
 }
 </script>
-
-<style scoped>
-
-</style>
