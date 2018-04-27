222732cf702205dc0ad4228f2e3171a5959a0dab
John Cleaver <cleaver.john.k@gmail.com>
2018-04-26 22:53:19 -0400

    Fix spacing and layout.

--- src/components/HeaderBar.vue
+++ src/components/HeaderBar.vue
@@ -1,31 +1,34 @@
 <template>
   <section class="hero is-info is-bold">
       <nav class="navbar">
-        <div class="navbar-brand">
-          <a class="navbar-item" href="/">
-            <h1 class="title is-hidden-mobile">Stardew Community Checklist</h1>
-            <h1 class="title is-hidden-tablet">SCC</h1>
-          </a>
-          <div class="navbar-burger"
-               v-bind:class="{ 'is-active': menu_active }"
-               @click="menu_active = !menu_active"
-          >
-            <span></span>
-            <span></span>
-            <span></span>
+        <div class="container">
+          <div class="navbar-brand">
+            <a class="navbar-item" href="/">
+              <h1 class="is-size-3 has-text-weight-semibold is-hidden-mobile">Stardew Community Checklist</h1>
+              <h1 class="is-size-3 has-text-weight-semibold is-hidden-tablet">SCC</h1>
+            </a>
+            <div class="navbar-burger"
+                v-bind:class="{ 'is-active': menu_active }"
+                @click="menu_active = !menu_active"
+            >
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
           </div>
-        </div>
-        <div class="navbar-menu"
-          v-bind:class="{ 'is-active': menu_active }">
-          <div class="navbar-end">
-            <router-link v-for="route in this.$router.options.routes"
-                         :key="route.order"
-                         class="navbar-item"
-                         v-bind:class="{ 'is-active': menu_active }"
-                         :to="route">
-              {{route.name}}
-            </router-link>
+          <div class="navbar-menu"
+            v-bind:class="{ 'is-active': menu_active }">
+            <div class="navbar-end">
+              <router-link v-for="route in named_routes"
+                          :key="route.order"
+                          class="navbar-item"
+                          @click.native="menu_active = false"
+                          :to="route">
+                {{route.name}}
+              </router-link>
+            </div>
           </div>
+
         </div>
       </nav>
   </section>
@@ -37,6 +40,11 @@ export default {
   data: function () {
     return {
       menu_active: false
+    }
+  },
+  computed: {
+    named_routes: function () {
+      return this.$router.options.routes.filter(route => route.hasOwnProperty('name'))
     }
   }
 }
