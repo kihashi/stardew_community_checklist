010af5ea499086631f1927b817727c24d80036a2
John Cleaver <cleaver.john.k@gmail.com>
2018-04-26 22:54:17 -0400

    Add new routes.

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,44 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Welcome from '@/components/Welcome'
+import Bundles from '@/components/Bundles'
+import BundleItems from '@/components/BundleItems'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/welcome',
       name: 'Welcome',
       component: Welcome
+    },
+    {
+      path: '/',
+      redirect: {
+        name: 'Welcome'
+      }
+    },
+    {
+      path: '/bundles',
+      name: 'Bundles',
+      component: Bundles,
+      children: [
+        {
+          path: ':id',
+          name: 'bundle-items',
+          component: BundleItems
+        }
+      ]
+    },
+    {
+      name: 'Search',
+      path: '/search'
+    },
+    {
+      name: 'Inventory',
+      path: '/inventory'
     }
   ],
-  linkExactActiveClass: 'is-active'
+  linkActiveClass: 'is-active'
 })
