5f7dcaa4c31629c8b97764d08b3573786da6d959
John Cleaver <cleaver.john.k@gmail.com>
2018-04-26 22:54:09 -0400

    Add component to display bundle contents.

---
+++ src/components/BundleItems.vue
@@ -0,0 +1,36 @@
+<template>
+<div id="bundle-items">
+    <section class="container">
+        <h3 class="title">
+          {{bundle.name}}
+        </h3>
+        <h5 class="subtitle">
+          {{bundle.reward}}
+        </h5>
+        <!-- Room Progress -->
+        <!-- Bundle Progress -->
+    <!-- </section>
+    <section class="section"> -->
+        <div class="columns is-multiline">
+          <div class="column is-3 is-flex" v-for="bundleitem in bundle.items" :key="bundleitem.id">
+            <item-card :item="bundleitem.item"></item-card>
+          </div>
+        </div>
+    </section>
+</div>
+</template>
+
+<script>
+import ItemCard from '@/components/item_card/ItemCard'
+export default {
+  name: 'bundle-items',
+  computed: {
+    bundle: function () {
+      return this.$store.getters.GetBundleById(this.$route.params.id)
+    }
+  },
+  components: {
+    ItemCard
+  }
+}
+</script>
