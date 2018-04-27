213e1d74b7756c26e408614c675977839308eec3
John Cleaver <cleaver.john.k@gmail.com>
2018-04-26 22:51:25 -0400

    Add logic to load bundles

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,32 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import SkillJson from '@/assets/game_data/skills.json'
+import SeasonJson from '@/assets/game_data/seasons.json'
+import RoomJson from '@/assets/game_data/rooms.json'
+import BundleJson from '@/assets/game_data/bundles.json'
+import ItemJson from '@/assets/game_data/items.json'
+
 Vue.use(Vuex)
 
+var state = {
+  StoredItems: {},
+  seasons: loadSeasons(),
+  skills: loadSkills(),
+  rooms: loadRooms()
+}
+
+state.bundles = loadBundles(state.rooms)
+state.items = loadItems(state.bundles, state.skills, state.seasons)
+
 export default new Vuex.Store({
-  state: {
-    StoredItems: {}
-  },
+  state: state,
   getters: {
     IsBundleItemRedeemed: (state) => (BundleItem) => {
       return state.StoredItems.hasOwnProperty(BundleItem.id)
+    },
+    GetBundleById: (state) => (bundleId) => {
+      return getById(state.bundles, bundleId)
     }
   },
   mutations: {
@@ -21,3 +38,45 @@ export default new Vuex.Store({
     }
   }
 })
+
+function getById (array, id) {
+  return array.find(x => x.id === id)
+}
+
+function loadSeasons () {
+  return SeasonJson.seasons
+}
+
+function loadSkills () {
+  return SkillJson.skills
+}
+
+function loadRooms () {
+  return RoomJson.rooms.map(room => {
+    room.bundles = new Array(0)
+    return room
+  })
+}
+
+function loadBundles (rooms) {
+  return BundleJson.bundles.map(bundle => {
+    bundle.room = getById(rooms, bundle.room)
+    bundle.room.bundles.push(bundle)
+    bundle.items = new Array(0)
+    return bundle
+  })
+}
+
+function loadItems (bundles, skills, seasons) {
+  return ItemJson.items.map(item => {
+    item.skills = item.skills.map(skill => getById(skills, skill))
+    item.seasons = item.seasons.map(season => getById(seasons, season))
+    item.bundles.map(bundleItem => {
+      bundleItem.bundle = getById(bundles, bundleItem.bundle_id)
+      bundleItem.item = item
+      bundleItem.bundle.items.push(bundleItem)
+      return bundleItem
+    })
+    return item
+  })
+}
