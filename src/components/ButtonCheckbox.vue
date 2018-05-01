5db60dd09bf719f54498da2b987479941b3fb262
John Cleaver <cleaver.john.k@gmail.com>
2018-04-30 22:44:59 -0400

    Add a button checkbox component

---
+++ src/components/ButtonCheckbox.vue
@@ -0,0 +1,36 @@
+<template>
+  <button class="button is-rounded"
+    :class="checked ? checked_class : unchecked_class"
+    v-on:click="click"
+    >
+      <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'button-checkbox',
+  props: {
+    checked_class: {
+      default: 'is-info'
+    },
+    unchecked_class: {
+      default: ''
+    }
+  },
+  data () {
+    return {
+      checked: true
+    }
+  },
+  methods: {
+    click () {
+      this.checked = !this.checked
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
