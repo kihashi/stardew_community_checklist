7a14798900828218bf9729a95f38ce50ee58cd8f
John Cleaver <cleaver.john.k@gmail.com>
2019-03-09 10:56:47 -0500

    Add clipboard package

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueClipboard from 'vue-clipboard2'
 import App from './App'
 import router from './router'
 import store from './store'
@@ -8,6 +9,7 @@ import store from './store'
 require('./assets/sass/main.scss')
 
 Vue.config.productionTip = false
+Vue.use(VueClipboard)
 
 /* eslint-disable no-new */
 new Vue({
