e030c7496ba1895bd9016753dbb42c03890a8277
John Cleaver <cleaver.john.k@gmail.com>
2019-03-11 21:45:00 -0400

    Convert v1 data on load

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,12 @@ export default {
   },
   created () {
     this.$store.commit('initState')
+    var v1data = localStorage.getItem('user_data')
+    if (v1data !== null && v1data !== '') {
+      this.$store.dispatch('LoadV1State', JSON.parse(atob(v1data)))
+      localStorage.removeItem('user_data')
+      localStorage.setItem('v1data', v1data)
+    }
   }
 }
 </script>
