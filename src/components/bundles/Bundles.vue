8b2687f0510923cb4f8ec275eccf8dc83ae58295
John Cleaver <cleaver.john.k@gmail.com>
2019-12-04 23:02:12 -0500

    Add page and bundle transitions

--- src/components/bundles/Bundles.vue
+++ src/components/bundles/Bundles.vue
@@ -2,7 +2,12 @@
   <div>
     <BundleNav />
     <section class="section">
-      <router-view></router-view>
+      <transition
+        name="fade"
+        mode="out-in"
+      >
+        <router-view :key="bundle"></router-view>
+      </transition>
     </section>
   </div>
 </template>
@@ -11,6 +16,23 @@
 import BundleNav from '@/components/bundles/BundleNav.vue'
 export default {
   components: {BundleNav},
-  name: 'bundles'
+  name: 'bundles',
+  computed: {
+    bundle: function () {
+      return this.$store.getters.GetBundleById(this.$route.params.id)
+    }
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter, .fade-leave-active {
+  opacity: 0;
+}
+
+</style>
