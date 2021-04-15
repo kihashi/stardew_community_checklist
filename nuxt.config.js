990a67e177cd695b101c5aa5da2c094dddd0b98f
John Cleaver <cleaver.john.k@gmail.com>
2021-04-15 16:48:22 -0400

    Update title

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'stardew_community_checklist',
+    title: 'Stardew Community Checklist',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
